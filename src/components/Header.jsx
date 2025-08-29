d1e59c4285041c82d7de752588f08093dc1d71ef
Ricardo <ricardorpolser@gmail.com>
2025-08-29 19:32:07 +0100

    Header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logo from "../assets/logo.png";
+
 export default function Header() {
   return (
     <header id="header">
       <img
-        src="/investment-calculator-logo.png"
+        src={logo}
         alt="Big green bag with dollar sign and coins stacked around it"
       ></img>
       <h1>Investment Calculator</h1>
